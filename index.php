ce0f8371ca7706b3c44032ca2c2a2d00f44eee1a
mpexabi <mpexabi@gmail.com>
2019-03-23 15:33:08 +0700

    ww

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
     $db = "webdatabase";
 
     try {
-        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
