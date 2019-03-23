701cbcd8372fce90873aff64eb98b7e5d91fff76
mpexabi <mpexabi@gmail.com>
2019-03-23 15:06:06 +0700

    fixx

--- index.php
+++ index.php
@@ -53,63 +53,62 @@
 </head>
 
 <body>
-    <h1>Data Pribadi!</h1>
-    <p>Masukkan nama, Alamat dan kota anda lalu tekan <strong>Submit</strong> untuk registrasi.</p>
+    <h1>Register here!</h1>
+    <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
     <form method="post" action="index.php" enctype="multipart/form-data">
         Name <input type="text" name="name" id="name" /></br></br>
-        Alamat <input type="text" name="alamat" id="alamat" /></br></br>
-        Kota <input type="text" name="kota" id="kota" /></br></br>
+        Email <input type="text" name="email" id="email" /></br></br>
+        Job <input type="text" name="job" id="job" /></br></br>
         <input type="submit" name="submit" value="Submit" />
         <input type="submit" name="load_data" value="Load Data" />
     </form>
 
     <?php
-    $host = "ferywebserver";
-    $user = "Fery";
-    $pass = "1234Qwer";
-    $db = "webdatabase";
-
+    $host = "<Nama server database Anda>";
+    $user = "<Nama admin database Anda>";
+    $pass = "<Password admin database Anda>";
+    $db = "<Nama database Anda>";
     try {
-        $conn = new PDO($host, $db, $user, $pass);
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $e) {
         echo "Failed: " . $e;
     }
-
     if (isset($_POST['submit'])) {
         try {
-            $name = $_POST['nama'];
-            $alamat = $_POST['alamat'];
-            $kota = $_POST['kota'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $job = $_POST['job'];
+            $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO registrasi (nama, alamat, kota) 
+            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $alamat);
-            $stmt->bindValue(3, $kota);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $job);
+            $stmt->bindValue(4, $date);
             $stmt->execute();
         } catch (Exception $e) {
             echo "Failed: " . $e;
         }
-
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM registrasi";
+            $sql_select = "SELECT * FROM Registration";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll();
             if (count($registrants) > 0) {
                 echo "<h2>People who are registered:</h2>";
                 echo "<table>";
                 echo "<tr><th>Name</th>";
-                echo "<th>Alamat</th>";
-                echo "<th>Kota</th>";
-
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
                 foreach ($registrants as $registrant) {
                     echo "<tr><td>" . $registrant['name'] . "</td>";
-                    echo "<td>" . $registrant['alamat'] . "</td>";
-                    echo "<td>" . $registrant['kota'] . "</td>";
+                    echo "<td>" . $registrant['email'] . "</td>";
+                    echo "<td>" . $registrant['job'] . "</td>";
                     echo "<td>" . $registrant['date'] . "</td></tr>";
                 }
                 echo "</table>";
